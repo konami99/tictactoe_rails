0b7462d443befa5e924beb4c62aef13ba39e0afa
Richard Chou <richard@ezyraise.com>
2023-06-24 18:16:05 +1000

    added RAILS_MASTER_KEY

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module TictactoeRails
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts = [
+      /.*/,
+    ]
   end
 end
